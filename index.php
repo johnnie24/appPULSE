4654d957b5233369adc68398731c28799085156b
johnnie24 <31224324+johnnie24@users.noreply.github.com>
2017-11-16 08:21:44 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>usando componentes</title>
+  <script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+  <!--link rel="import" href="elements/saludo-polymer/saludo-polymer.html"-->
+  <link rel="import" href="bower_components/paper-button/paper-button.html">
+  <link rel="import" href="elements/project-chart/project-chart.html">
+</head>
+<body>
+<paper-button raised>HOLA</paper-button>
+<project-chart>EQUIPO PERRON</project-chart>
+</body>
+</html>
